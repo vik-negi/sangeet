53197b33cac7d936ae51f4ff0763cceefeb1f9a3
vikram negi <vikramnegi175@gmail.com>
2023-04-23 20:53:15 +0530

    sangeet updated

--- MVPlayer/settings.py
+++ MVPlayer/settings.py
@@ -101,10 +101,14 @@ WSGI_APPLICATION = 'MVPlayer.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': '',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    
+    'default' : {
+    'ENGINE' : 'django.db.backends.mysql',
+    'NAME' : os.environ['DB_NAME'],
+    'USER' : os.environ['DB_USER'],
+    'PASSWORD' : os.environ['DB_PASSWORD'],
+    'HOST' : 'localhost',
+    'PORT' : '3306',
     }
 }
 
@@ -178,12 +182,12 @@ SITE_ID = 1
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # LOGIN_URL = 'login'
 # LOGOUT_URL = 'logout'
